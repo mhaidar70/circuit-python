8e7a6fd8b3923c9fc9460aed0265e275d939e946
Mohammad Haidari <112962044+mhaidar70@users.noreply.github.com>
2022-09-23 11:27:46 -0400

    Add files via upload
    Ultrasonic code to change the neopixel colors smoothly.

---
+++ ultrasonic code.py
@@ -0,0 +1,38 @@
+import time
+import board
+import neopixel
+import adafruit_hcsr04
+import simpleio                     #imported lib
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D11, echo_pin=board.D12)
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.2               #setting up ultrasonic and neopixel
+
+while True:
+    try:
+        cm=int(sonar.distance)      #defining cm as a variable of what the ultrasonic sensor is reading
+        print(cm)
+        if cm<5: 
+            dot.fill((255, 0, 0))   #if the distance is less than 5 cm, red
+            time.sleep(.15)
+        elif 5<=cm<20:
+            red=simpleio.map_range(sonar.distance,5,20,255,0)
+            green=simpleio.map_range(sonar.distance,5,20,0,0)
+            blue=simpleio.map_range(sonar.distance,5,20,0,255)
+            dot.fill((red,green,blue))                   #smoothly trasition from red to blue as the distance goes from 5cm to 20cm
+            time.sleep(.05)
+        elif 20<=cm<=35:   
+            red=simpleio.map_range(sonar.distance,20,35,0,0)
+            green=simpleio.map_range(sonar.distance,20,35,0,255)
+            blue=simpleio.map_range(sonar.distance,20,35,255,0)
+            dot.fill((red, green, blue))                 #smoothly trasition from blue to green as the distance goes from 20cm to 35cm
+            time.sleep(.05)
+        elif cm>35:
+            dot.fill((0, 255, 0))                        #if the distance is more than 35 cm, turn the light green
+            time.sleep(.15)
+        else:
+            dot.fill((0, 0, 0))                          #if there is no reading, off
+            time.sleep(.15)
+    except RuntimeError:                                 #if there is no numbers then tell me
+        print("Retrying!")
+    time.sleep(0.1)                                      #debounce
